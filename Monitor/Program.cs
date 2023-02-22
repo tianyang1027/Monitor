e9ab26cd772cb3921f05ac4a7dd736663c7a8875
Yang Tian <v-yangtian@micosoft.com>
2023-02-22 16:09:29 +0800

    Add project files.

---
+++ Monitor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Monitor
+{
+    internal class Program
+    {
+        private static string lastWriteTime { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString());
+            Thread thread = new Thread(new ThreadStart(Monitor));
+            thread.Start();
+        }
+
+        static void Monitor()
+        {
+            while (true)
+            {
+                var fileLastWriteTime = File.GetLastWriteTime(@"C:\Users\v-yangtian\Desktop\FileTest.txt");
+                Console.WriteLine(DateTime.Now.ToString() + "_" + fileLastWriteTime.ToString());
+                if (fileLastWriteTime.ToString() != lastWriteTime)
+                {
+                    lastWriteTime = fileLastWriteTime.ToString();
+                }
+                Thread.CurrentThread.Join(1000);
+            }
+        }
+    }
+}
